7f55ecdf3cf2dca9b22408be1df7b0c388ffdf19
toptiop <49591639+toptiop@users.noreply.github.com>
2024-02-22 18:16:10 +0700

    Fix Box

--- ProejectThesis/Assets/Scripts/Door/DoorActive.cs
+++ ProejectThesis/Assets/Scripts/Door/DoorActive.cs
@@ -19,7 +19,7 @@ public class DoorActive : MonoBehaviour
     {
         isDelaying = false;
         source.PlayOneShot(clip);
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.2f);
         anim.SetBool("isOpen", newOpen);
     }
 }
