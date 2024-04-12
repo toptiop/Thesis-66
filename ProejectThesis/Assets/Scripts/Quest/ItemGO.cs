2ca0efd92d7fd2cdbf08c5a01e53dbefcd7d7d86
Mymjthanakrit <mhokspeed1@gmail.com>
2024-04-12 17:05:37 +0700

    DialogueFix

--- ProejectThesis/Assets/Scripts/Quest/ItemGO.cs
+++ ProejectThesis/Assets/Scripts/Quest/ItemGO.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class ItemGO : MonoBehaviour
 {
+    public bool isnextQuest;
+    public SO_Quest nextQuest;
     public void UpdateQuestProgress(ScriptableObject data)
     {
         QuestManager.instance.UpdateQuestProgress(ObjectiveType.Gather, data);
+
+        if (isnextQuest)
+        {
+            QuestManager.instance.AcceptQuest(nextQuest);
+        }
     }
+
 }
