78b3acd7bc5199b943ba165d13c6d81d7f5e4ccd
Mymjthanakrit <mhokspeed1@gmail.com>
2024-04-16 18:15:05 +0700

    PuzzleNate03

---
+++ ProejectThesis/Assets/Scripts/GamePlay/Platform.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(this.transform);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
